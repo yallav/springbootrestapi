9e57958ea400f13b6c9dcd984a6da177a38e0619
vijay <vijay@vijays-MacBook-Pro.local>
2019-01-03 17:02:12 +0530

    Updated the files with put, delete and get methods

--- src/main/java/com/restapi/topics/controller/TopicController.java
+++ src/main/java/com/restapi/topics/controller/TopicController.java
@@ -3,14 +3,26 @@ package com.restapi.topics.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restapi.topics.model.Topic;
+import com.restapi.topics.service.ResourceNotFoundException;
 import com.restapi.topics.service.TopicService;
+
+/**
+ * 
+ * @author vijay
+ * This controller takes care request mapping and invokes corresponding method from TopicService
+ *
+ */
 
 @RestController
 @RequestMapping("/api")
@@ -25,8 +37,29 @@ public class TopicController {
 		return topicService.getTopics();
 	}
 
+	@GetMapping("/topics/{id}")
+	public ResponseEntity<Topic> getTopic(@PathVariable(value = "id") String topicId) 
+		throws ResourceNotFoundException 
+	{
+		return topicService.getTopic(topicId);
+	}
+	
 	@PostMapping("/topics")
 	public void createTopic(@RequestBody Topic topic) {
 		topicService.addTopic(topic);
+	}
+	
+	@PutMapping("/topics/{id}")
+	public void updateTopic(@PathVariable(value = "id") String topicId, @RequestBody Topic topic) 
+			throws ResourceNotFoundException 
+	{
+		topicService.updateTopic(topicId, topic);
+	}
+	
+	@DeleteMapping("/topics/{id}")
+	public void removeTopic(@PathVariable(value = "id") String topicId) 
+			throws ResourceNotFoundException 
+	{
+		topicService.deleteTopic(topicId);
 	}
 }
--- src/main/java/com/restapi/topics/service/TopicService.java
+++ src/main/java/com/restapi/topics/service/TopicService.java
@@ -1,12 +1,23 @@
 package com.restapi.topics.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.restapi.topics.model.Topic;
 import com.restapi.topics.repository.TopicRepository;
+
+/**
+ * 
+ * @author vijay
+ * This service takes care of operations with MySql table Topic with the help of TopicRepository.
+ * Intention of this service is to have complete business logic.
+ *
+ */
 
 @Service
 public class TopicService {
@@ -15,13 +26,43 @@ public class TopicService {
 	@Autowired
 	TopicRepository topicsRepository;
 	
-	public List<Topic> getTopics() {
-		
+	public List<Topic> getTopics() {	
 		return topicsRepository.findAll();
 	}
 
+	public ResponseEntity<Topic> getTopic(String topicId) 
+			throws ResourceNotFoundException
+	{
+		Topic topic = topicsRepository.findById(topicId).orElseThrow(() -> new ResourceNotFoundException("Not found"));
+		return ResponseEntity.ok().body(topic);
+	}
+	
 	public void addTopic(Topic newTopic) {
 		topicsRepository.save(newTopic);
+	}
+	
+	public ResponseEntity<Topic> updateTopic(String topicId, Topic existedTopic) 
+			throws ResourceNotFoundException 
+	{
+		Topic topic = topicsRepository.findById(topicId).orElseThrow(() -> new ResourceNotFoundException("Not found"));
+		topic.setId(existedTopic.getId());
+		topic.setName(existedTopic.getName());
+		topic.setDescription(existedTopic.getDescription());
 		
+		topicsRepository.save(existedTopic);
+		return ResponseEntity.ok().body(topic);		
+	}
+	
+	public Map<String, Boolean> deleteTopic(String topicId) 
+			throws ResourceNotFoundException 
+	{
+		topicsRepository.findById(topicId).orElseThrow(() -> new ResourceNotFoundException("Not found"));
+		
+		topicsRepository.deleteById(topicId);
+		
+		Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        
+        return response;
 	}
 }
